294a38582d892b4bdf6ac4eaf9839003b83d07f3
Tar Zoltán <tarzoltan531@gmail.com>
2023-12-05 08:02:12 +0100

    Implementing the Read functionality

--- src/main/java/web/webspringapp/controller/AuthorController.java
+++ src/main/java/web/webspringapp/controller/AuthorController.java
@@ -2,12 +2,17 @@ package web.webspringapp.controller;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import web.webspringapp.model.Author;
 import web.webspringapp.dto.AuthorDTO;
 import web.webspringapp.repository.AuthorRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class AuthorController {
@@ -26,5 +31,20 @@ public class AuthorController {
         Author author = modelMapper.map(authorDTO, Author.class);
         Author savedAuthor = authorRepository.save(author);
         return modelMapper.map(savedAuthor, AuthorDTO.class);
+    }
+
+    @GetMapping("/authors")
+    public ResponseEntity<List<AuthorDTO>> getAllAuthors() {
+        List<Author> authors = authorRepository.findAll();
+
+        if (authors.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        List<AuthorDTO> authorDTOs = authors.stream()
+                .map(author -> modelMapper.map(author, AuthorDTO.class))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(authorDTOs);
     }
 }
--- src/main/java/web/webspringapp/controller/NovelController.java
+++ src/main/java/web/webspringapp/controller/NovelController.java
@@ -2,12 +2,17 @@ package web.webspringapp.controller;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import web.webspringapp.dto.NovelDTO;
 import web.webspringapp.model.Novel;
 import web.webspringapp.repository.NovelRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class NovelController {
@@ -26,5 +31,20 @@ public class NovelController {
         Novel newNovel = modelMapper.map(novelDTO, Novel.class);
         Novel savedNovel = novelRepository.save(newNovel);
         return modelMapper.map(savedNovel, NovelDTO.class);
+    }
+
+    @GetMapping("/novels")
+    public ResponseEntity<List<NovelDTO>> getAllNovels() {
+        List<Novel> novels = novelRepository.findAll();
+
+        if (novels.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        List<NovelDTO> novelDTOs = novels.stream()
+                .map(novel -> modelMapper.map(novel, NovelDTO.class))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(novelDTOs);
     }
 }
